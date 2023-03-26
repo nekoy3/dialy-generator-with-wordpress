db302af653eac81b20dc1e1cebf3ac1ba784adcf
nekoy3 <nekoyama3.n@gmail.com>
2023-03-26 22:15:51 +0900

    クラス追加とスラッシュコマンド追加
    Discord上で何かしら操作をするためのメソッドをDiscordControllerクラスに宣言
    対話型で実際に実行時間が長くなる処理をAsynchronousMethodsクラスに宣言
    スラッシュコマンドはメソッドとしてBotClientに宣言しコンストラクタ内でtreeを定義した後にデコレータを付与する。

--- discordBot.py
+++ discordBot.py
@@ -1,27 +1,9 @@
 import json
 import discord
 from discord import app_commands
+import asyncio
 
-#キーワード引数
-#javadrive.jp/python/userfunc/index6.html
-class BotClient(discord.Client):
-    def __init__(self, guild_id, channel_id, wp_client):
-        intents = discord.Intents.all()
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self) #全てのコマンドを管理するCommandTree型オブジェクトを生成
-        self.guild_id = guild_id
-        self.channel_id = channel_id
-        self.wp_client = wp_client
-
-    async def setup_hook(self):
-        guild=discord.Object(id=self.guild_id)
-        self.tree.copy_global_to(guild=guild)
-        await self.tree.sync(guild=guild)
-    
-    def make_embed(self, title, description, color=0x000000):
-        embed = discord.Embed(title=title,description=description,color=color)
-        return embed
-    
+class DiscordController(discord.Client):
     async def send_message(self, msg):
         l = len(msg)
         msg_list = []
@@ -39,6 +21,44 @@ class BotClient(discord.Client):
         c = self.get_channel(self.channel_id)
         await c.send(embed=embed)
 
+
+#時間がかかる処理を行うメソッドをすべてこのクラスに格納する
+class AsynchronousMethods:
+    def __init__(self):
+        self.sub_obj = self.get_subclass_object()   # サブクラスのインスタンスを取得
+    
+    async def write_diary(self):
+        await asyncio.sleep(2)
+        await self.send_message("Yes")
+
+    #DiscordControllerにアクセスするためのメソッド
+    def get_subclass_object(self):
+        return self
+
+#キーワード引数
+#javadrive.jp/python/userfunc/index6.html
+class BotClient(DiscordController, AsynchronousMethods):
+    def __init__(self, guild_id, channel_id, wp_client):
+        intents = discord.Intents.all()
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self) #全てのコマンドを管理するCommandTree型オブジェクトを生成
+        self.guild_id = guild_id
+        self.channel_id = channel_id
+        self.wp_client = wp_client
+        self.async_methods = AsynchronousMethods
+
+        #デコレータを直接付与すると、コンストラクタ実行前に実行されnot definedになるためあと付けデコレータする
+        self.make_diary = self.tree.command(name="write_diary", description="日記の記述を開始します。")(self.make_diary)
+
+    async def setup_hook(self):
+        guild=discord.Object(id=self.guild_id)
+        self.tree.copy_global_to(guild=guild)
+        await self.tree.sync(guild=guild)
+    
+    def make_embed(self, title, description, color=0x000000):
+        embed = discord.Embed(title=title,description=description,color=color)
+        return embed
+
     async def on_ready(self):
         print("Bot ready.")
         """
@@ -46,3 +66,9 @@ class BotClient(discord.Client):
         e = self.make_embed("投稿テスト", f"投稿が完了しました。\nid: {p['id']}\ntitle: {p['title']['rendered']}\nlink: {p['link']}")
         await self.send_embed(e)
         """
+    
+    async def make_diary(self, interaction):
+        #コルーチン関数を非同期タスクで実行するにはawaitせずコルーチンオブジェクトとして受け取りタスクとして投げる
+        coro_obj=self.async_methods.write_diary(self)
+        asyncio.create_task(coro_obj)
+        await interaction.response.send_message("done")
