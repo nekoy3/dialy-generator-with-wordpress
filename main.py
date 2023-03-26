3fe5ec2f00e22780c3477330f24256b6d1b14c55
nekoy3 <nekoyama3.n@gmail.com>
2023-03-26 22:13:24 +0900

    MyBotクラスの削除
    不要のため

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from configIO import ConfigClass
 from connectWP import WpClass
 from discordBot import BotClient
 
-class MyBot:
-    def __init__(self, cfg, wp_client):
-        self.client = BotClient(cfg.guild_id, cfg.channel_id, wp_client)
-        print('Starting...')
-
 cfg = ConfigClass()
 wp_client = WpClass(cfg.url, cfg.username, cfg.password)
-mybot = MyBot(cfg, wp_client)
+bot_client = BotClient(cfg.guild_id, cfg.channel_id, wp_client)
+print('Starting...')
 
-mybot.client.run(cfg.token)
+bot_client.run(cfg.token)
