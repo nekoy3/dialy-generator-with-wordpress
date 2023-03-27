7bb0ce192fd54967dfc572128c29e07c70d56c58
nekoy3 <nekoyama3.n@gmail.com>
2023-03-28 02:02:38 +0900

    記事タイトルとスラッグ入力処理の実装
    テキスト読み取りをループで試みるread_text_loopメソッドの追加
    最終記述をオンメモリに保管するメソッドの追加
    入力内容によって分岐される処理をread_text_loop内に実装

--- discordBot.py
+++ discordBot.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 import asyncio
+from datetime import datetime
 
 class DiscordController(discord.Client):
     async def send_message(self, msg):
@@ -23,24 +24,66 @@ class DiscordController(discord.Client):
 
 #別タスクとして投げられるメソッドをすべてこのクラスに格納する
 class AsynchronousMethods:
-    async def write_diary(self):
-        kill_switch = False
-        title = ""
-        slug = ""
-        content = ""
+    def get_last_processed(self, d):
+        self.last_processed_datadict = d
 
-        #タイトル入力
-        await self.sub_obj.send_message("Please enter a title.")
+    async def read_text_loop(self, prompt, validator):
+        await self.sub_obj.send_message(prompt)
         while True:
-            #メッセージ応答を待機し、メッセージ、添付ファイル、強制終了フラグを返す。
+            # メッセージ応答を待機し、メッセージ、添付ファイル、強制終了フラグを返す。
             msg, _, kill_switch = await self.sub_obj.interrupt_msg_sleep()
-            if kill_switch: 
-                return
-            if msg != "":
-
+            if kill_switch:
+                return -1
             
-        #メッセージが入ったらsub
+            # if not "" (空文字列)はTrueと評価される。
+            if not msg:
+                continue
 
+            #passされたときは空のまま何もしない
+            if msg == "pass":
+                await self.sub_obj.send_message("skipped.")
+                break
+
+            #validatorはラムダ式
+            #lambda x: True if len(x) <= 文字数 else False
+            #という式を保持している。
+            #1, 引数(msg)をlambda xのxとして受け取る。
+            #2, 条件式評価し、Trueならifより前の値を返す。elseならFalseを返す。
+            if validator(msg):
+                return msg
+
+    async def write_diary(self):
+        self.last_processed_datadict = {}
+        input_datadict = {
+            'title': "",  # <32
+            'content': "",  # HTML
+            'slug': "",  # <200
+            'status': "draft",  # draft=下書き、publish=公開　省略時はdraftになる
+        }
+
+        # タイトル入力
+        title_prompt = "Please enter a title."
+        title_validator = lambda x: True if len(x) <= 32 else False
+        title = await self.read_text_loop(title_prompt, title_validator)
+        if title == -1:
+            self.get_last_processed(input_datadict)
+            return
+        input_datadict["title"] = title
+
+        # スラッグ入力
+        slug_prompt = "Please enter a slug.\n「diary」と入力すると自動で今日の日時[diary-yyyy-mm-dd]となります。"
+        slug_validator = lambda x: True if len(x) <= 200 else False
+        slug = await self.read_text_loop(slug_prompt, slug_validator)
+        if slug == -1:
+            self.get_last_processed(input_datadict)
+            return
+        if slug == "diary":
+            slug = datetime.today().strftime("%Y-%m-%d")
+            await self.sub_obj.send_message("slug -> " + slug)
+        input_datadict["slug"] = slug
+        
+        #本文入力
+        
 
 #キーワード引数
 #javadrive.jp/python/userfunc/index6.html
@@ -55,7 +98,7 @@ class BotClient(DiscordController):
         self.async_methods = AsynchronousMethods()
         #メッセージを取得したら保持する
         self.got_msg = ""
-        self.attachment_list = []
+        self.attachments_list = []
 
         #sub_objについて
         #sub_objはこのクラス自身を保持しており、AsynchronousMethosクラスのインスタンスが保持している。
