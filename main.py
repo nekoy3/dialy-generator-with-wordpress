b7deee54205cec4859f8c582faef967e69a9b827
nekoy3 <nekoyama3.n@gmail.com>
2023-04-18 14:25:58 +0900

    コメント行の追加

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from configIO import ConfigClass
 from connectWP import WpClass
 from discordBot import BotClient
 
+#*コンフィグを読み込んでインスタンス化し保持する
+#?config.iniを読み込んで項目を保持する。存在しなければ設定を促しプログラムを終了する。
 cfg = ConfigClass()
+
+#*WordPress関連の操作を行うインスタンスを生成
 wp_client = WpClass(cfg.url, cfg.username, cfg.password)
+
+#*discord botを操作するインスタンスを生成
 bot_client = BotClient(cfg.guild_id, cfg.channel_id, wp_client)
 print('Starting...')
 
+#*botの実行開始
+#!この処理以降は実行されない。
 bot_client.run(cfg.token)
